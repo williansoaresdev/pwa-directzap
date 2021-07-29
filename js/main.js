a0715b95dc6dd13688b4fcb891b2f91fef7c8067
Willian Soares <contact@williansoaresdev.com>
2021-07-29 13:48:24 -0300

    Testing a splash screen
    Testing a splash screen that slide up after 2 seconds.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 /**
  * Código motor da aplicação
 */
+
+(function(){
+    $(document).ready(function(){
+        setTimeout(function(){
+            $("#splashScreen").slideUp("normal",function(){
+
+            });
+        },2000);
+    });
+})();
