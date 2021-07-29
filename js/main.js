4036eec7bcc31e8946a226dbb301a51dc417d7e7
Willian Soares <contact@williansoaresdev.com>
2021-07-29 15:02:40 -0300

    Testing the whatsapp call
    Testing the whatsapp call trought the button

--- js/main.js
+++ js/main.js
@@ -8,7 +8,13 @@
             $("#splashScreen").slideUp("normal",function(){
                 $("#mainScreen").fadeIn("normal",function(){
 
+                    var noMask = function(content) {
+                        return content.replace(/\D/g, '');
+                    }
+
                     var validNumber = function(number) {
+                        $("#phoneNumber").removeClass("bordered");
+
                         if ((number.length < 10) || (number.length > 11)) {
                             $("#phoneNumber").addClass("bordered");
                             $("#phoneNumber").focus();
@@ -20,7 +26,7 @@
 
                     $("#btnJustGo").click(function(){
 
-                        var newNumber = $.trim($("#phoneNumber").val());
+                        var newNumber = noMask($.trim($("#phoneNumber").val()));
 
                         if (!validNumber(newNumber))
                             return;
@@ -28,6 +34,16 @@
                         $("#phoneNumber").attr("disabled","true");
                         $("#btnMemoGo").attr("disabled","true");
                         $("#btnJustGo").attr("disabled","true");
+
+                        $("#mainTitle").html("Redirecting...");
+
+                        setTimeout(function(){
+                            var newWindow = window.open("https://api.whatsapp.com/send?phone=55"+newNumber+"&text=");
+                            $("#mainTitle").html("Who you gonna call?");
+                            $("#phoneNumber").removeAttr("disabled");
+                            $("#btnMemoGo").removeAttr("disabled");
+                            $("#btnJustGo").removeAttr("disabled");
+                        },1000);
 
                     });
 
