412d2257d73e9622fdbef622ad28566f8ba4b04b
Willian Soares <contact@williansoaresdev.com>
2021-07-29 14:46:28 -0300

    Testing the phone number
    Testing the phone number with 10 or 11 digits

--- js/main.js
+++ js/main.js
@@ -7,6 +7,30 @@
         setTimeout(function(){
             $("#splashScreen").slideUp("normal",function(){
                 $("#mainScreen").fadeIn("normal",function(){
+
+                    var validNumber = function(number) {
+                        if ((number.length < 10) || (number.length > 11)) {
+                            $("#phoneNumber").addClass("bordered");
+                            $("#phoneNumber").focus();
+                            return false;
+                        }
+
+                        return true;
+                    }
+
+                    $("#btnJustGo").click(function(){
+
+                        var newNumber = $.trim($("#phoneNumber").val());
+
+                        if (!validNumber(newNumber))
+                            return;
+
+                        $("#phoneNumber").attr("disabled","true");
+                        $("#btnMemoGo").attr("disabled","true");
+                        $("#btnJustGo").attr("disabled","true");
+
+                    });
+
                     $("#phoneNumber").focus();
                 });
             });
