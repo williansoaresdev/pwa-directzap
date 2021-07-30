76b8f2adc6f47f286ab131c037d826dd5f893b4a
Willian Soares <contact@williansoaresdev.com>
2021-07-30 10:51:35 -0300

    Another Service Worker
    Testing an another Service Worker coding.

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
- self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('dir-zap').then((cache) => cache.addAll([
+var cacheName = 'x-dir-zap';
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll([
       '/pwa-directzap/',
       '/pwa-directzap/index.html',
       '/pwa-directzap/bootstrap/css/bootstrap.min.css',
@@ -31,13 +36,19 @@
       '/pwa-directzap/img/logo1.png',
       '/pwa-directzap/jquery/jquery-1.12.4.min.js',
       '/pwa-directzap/js/main.js'
-    ])),
-  );
+    ]);
+  })());
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
